6842cc9b3b9d73c368d8b2bfdb76c24e9c983359
orbitkorbi <craftorbit@ymail.com>
2015-10-11 17:08:50 +0200

    Upload project.

---
+++ android/src/orbit/main/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package orbit.main.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import orbit.main.GameSkeletonInit;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameSkeletonInit(), config);
+	}
+}
